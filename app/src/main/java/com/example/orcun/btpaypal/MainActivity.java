5b4d16195694cb88fc7389ec0fcca82d9b5fbc57
orcunkeskin <okeskin@live.ie>
2018-05-21 13:51:51 +0100

    Removing an unused variable

--- app/src/main/java/com/example/orcun/btpaypal/MainActivity.java
+++ app/src/main/java/com/example/orcun/btpaypal/MainActivity.java
@@ -33,7 +33,6 @@ import java.util.Map;
 
 public class MainActivity extends AppCompatActivity implements PaymentMethodNonceCreatedListener {
 
-    final int REQUEST_CODE = 1;
     final String get_token = "http://orcodevbox.co.uk/BTOrcun/tokenGen.php";
     final String send_payment_details = "http://orcodevbox.co.uk/BTOrcun/iosPayment.php";
     String token, amount, nonce;
